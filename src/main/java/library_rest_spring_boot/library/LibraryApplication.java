4adfb1babe7bfc6dd5c571cbf5831d388481d7fb
HendrylXamann <estudos123h@gmail.com>
2024-07-15 09:52:03 -0300

    initial commit

---
+++ src/main/java/library_rest_spring_boot/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package library_rest_spring_boot.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/library_rest_spring_boot/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package library_rest_spring_boot.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
